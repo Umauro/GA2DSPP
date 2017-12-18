e85f0d407349b614c8ad66df817eb92386a53c45
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2017-12-17 23:26:21 -0300

    Copyright BLF

--- src/individuo.cpp
+++ src/individuo.cpp
@@ -1,3 +1,19 @@
+//------------------------------------------------------------------------------
+// C++ BLF algorithm.
+//
+// Copyright (c) 2012 Charles Dubout <charles.dubout@idiap.ch>
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as
+// published by the Free Software Foundation.
+//
+// It is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//------------------------------------------------------------------------------
+
+
 #include <iostream>
 #include <string>
 #include <algorithm>
